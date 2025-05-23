b07a2e9cb39c4ec9fe47b5ead843203f9ea174a9
ekianuruzuna <ekianuruzuna@gmail.com>
2025-05-23 12:05:05 -0300

    first commit

---
+++ src/main/java/NutriAPP/TpFinalPrograIiiApplication.java
@@ -0,0 +1,13 @@
+package NutriAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalPrograIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalPrograIiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NutriAPP/TpFinalPrograIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package NutriAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalPrograIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
