f7fdc8fa77a51a073261f56b0c6b9ad714677f8d
ekian <ekianuruzuna@gmail.com>
2025-11-19 21:56:52 -0300

    Agrego un metodo en authService para obtener el authority (rol) del usuario logeado

--- src/app/service/auth.ts
+++ src/app/service/auth.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Authority } from '../models/authority';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,8 @@ export class AuthService {
   private baseURL = 'http://localhost:8080/auth';
   protected readonly http = inject(HttpClient);
 
+
+  //AUTH METHODS
   login(username: string, password: string): Observable<any> {
     const body = { username, password }; // 👈 Debe coincidir con LoginRequest
     return this.http.post(`${this.baseURL}/login`, body);
@@ -18,6 +21,12 @@ export class AuthService {
   return this.http.post(`${this.baseURL}/registro`, data);
   }
 
+  getRole (){
+    return this.http.get<Authority>("http://localhost:8080/api/rol/obtener");
+  }
+
+
+  //LOCAL STORAGE
   saveToken(token: string): void {
     localStorage.setItem('token', token);
   }
