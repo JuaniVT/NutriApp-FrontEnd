0e621c640fc3fe37f91777720a2380d206898af3
ekian <ekianuruzuna@gmail.com>
2025-11-27 11:50:10 -0300

    Agrego el componente de alimentos de nuestra base de datos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,16 +7,18 @@
     import { NotFound } from './pages/not-found/not-found';
     import { ComidasFavoritasComponent } from './pages/comidas-favoritas/comidas-favoritas';
     import { ListarSolicitudes } from './pages/solicitudes/listar-solicitudes/listar-solicitudes';
+import { AlimentosBDD } from './pages/alimentos-bdd/alimentos-bdd';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},     //ruta por defecto
     {path: "home", title: "Home", component: Home},
-    {path: 'login', component: InicioSesionComponent},
+    {path: 'login', title: "Login", component: InicioSesionComponent},
     {path: "perfil", title: "Perfil" , component: MiPerfilComponente},
-    {path: 'registro', component: RegistroComponent},
-    {path: 'dia/:fecha', component: VerDiaComponent},
-    {path: 'favoritas', component: ComidasFavoritasComponent},
-    {path: "listar-solicitudes/:mode", component: ListarSolicitudes},
+    {path: 'registro', title: "Registro", component: RegistroComponent},
+    {path: 'dia/:fecha', title: "Dia", component: VerDiaComponent},
+    {path: 'favoritas', title:"Comidas Favoritas", component: ComidasFavoritasComponent},
+    {path: "listar-solicitudes/:mode", title: "Solicitudes", component: ListarSolicitudes},
+    {path: "alimentos-bdd", title: "Alimentos BDD", component: AlimentosBDD},
 
         // !!IMPORTANTE¡¡ -> esta ruta tiene que estar al final de todas ya que sino, desde este componente las que estan abajo no te redireccionan
         {path: "**", title: "Not Found 404", component: NotFound}      //ruta de 404 (pagina no encontrada)
---
+++ src/app/pages/alimentos-bdd/alimentos-bdd.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alimentos-bdd',
+  imports: [],
+  templateUrl: './alimentos-bdd.html',
+  styleUrl: './alimentos-bdd.css',
+})
+export class AlimentosBDD {
+
+}
